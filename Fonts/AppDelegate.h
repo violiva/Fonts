2e6afc2bbe2b623c26fdaff25f920c0d2fbe5f16
Vicente Oliva <vicente_Oliva@hotmail.com>
2015-02-14 11:52:27 +0100

    Initial Commit

---
+++ Fonts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fonts
+//
+//  Created by Vicente Oliva de la Serna on 14/2/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
